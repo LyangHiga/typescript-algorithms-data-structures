0afd34c93aa1ef3c16990e3039defc4f846efe30
LyangHiga <lyang_higa@poli.ufrj.br>
2019-12-05 19:46:23 -0300

    Recursive Depth First Search

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -3,7 +3,7 @@ class Graph {
         this.list = {};
     };
     
-    // adds a empty array to the new vertex
+    // adds a empty array to the new v
     addVertex(v){
         if(!this.list[v]) this.list[v] = [];
         return this;
@@ -32,17 +32,60 @@ class Graph {
         delete this.list[v];
         return this;
     }
+
+    // Recursive Depth-First Search
+    dsfr(s){
+        const result = [];
+        const visited = {};
+        const list = this.list;
+        (function dfs(v){
+            if(!v) return;
+            visited[v] = true;
+            result.push(v);
+            list[v].forEach(u => {
+                if(!visited[u]){
+                    return dfs(u);
+                }
+            });
+        })(s);
+        return result;
+    }
 }
 
+// let g = new Graph();
+// console.log(g.addVertex("Dallas"));
+// console.log(g.addVertex("Tokyo"));
+// console.log(g.addVertex("Aspen"));
+// console.log(g.addEdge("Aspen","Tokyo"));
+// console.log(g.addEdge("Aspen","Dallas"));
+// console.log(g.addEdge("Tokyo","Dallas"));
+// // console.log(g.removeEdge("Aspen","Tokyo"));
+// console.log(g.removeVertex("Aspen"));
+
 let g = new Graph();
-console.log(g.addVertex("Dallas"));
-console.log(g.addVertex("Tokyo"));
-console.log(g.addVertex("Aspen"));
-console.log(g.addEdge("Aspen","Tokyo"));
-console.log(g.addEdge("Aspen","Dallas"));
-console.log(g.addEdge("Tokyo","Dallas"));
-// console.log(g.removeEdge("Aspen","Tokyo"));
-console.log(g.removeVertex("Aspen"));
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
 
 
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
+console.log(g.dsfr("A"));
 
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
