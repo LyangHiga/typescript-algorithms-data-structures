49e5e0e2a671e9c7fed209fb26093739472a8f0d
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-04 17:26:19 -0300

    Add minHeap size property

--- data-structures/minHeap.js
+++ data-structures/minHeap.js
@@ -10,32 +10,26 @@ class MinHeap {
     this.values = [];
     // dict: key to array idx => you say the key it returns the idx
     this.idxs = {};
+    this.size = 0;
   }
 
-  //   return true if this heap constains this key
-  //   otherwise returns false
-  contains(key) {
-    if (this.values[this.idxs[key]] === undefined) return false;
-    return true;
-  }
-
-  // return true if the element from index i is smaller than k idx element
+  // returns true if the element from index i is smaller than k idx element
   lessThan(i, k) {
     // out of bounds
     if (i < 0 || k < 0) return false;
-    if (i > this.values.length - 1 || k > this.values.length - 1) return false;
+    if (i > this.size - 1 || k > this.size - 1) return false;
     let val = this.values[i].val;
     let parent = this.values[k].val;
     if (val < parent) return true;
     return false;
   }
 
-  //   return the parent's index of the ith node
+  //   returns the parent's index of the ith node
   myParentIdx(i) {
     return Math.floor((i - 1) / 2);
   }
 
-  //   return the children's index of the ith node
+  //   returns the children's index of the ith node
   myChildrenIdx(i) {
     // left 2 * i + 1 , right 2 * idx + 2
     return [2 * i + 1, 2 * i + 2];
@@ -77,16 +71,24 @@ class MinHeap {
     return smallIdx;
   }
 
+  //   return true if this heap constains this key
+  //   otherwise returns false
+  contains(key) {
+    if (this.values[this.idxs[key]] === undefined) return false;
+    return true;
+  }
+
   // insert an element in the next free spot and rearrange
-  //   return Heap
+  // returns Heap
   enqueue(key, val) {
     // check whether this val belongs to this heap
     // to avoid duplicate keys
     if (this.contains(key)) return false;
     let node = new Node(key, val);
     this.values.push(node);
+    this.size++;
     // last position to insert this new node
-    let idx = this.values.length - 1;
+    let idx = this.size - 1;
     // add the idx of this key on the dict
     this.idxs[key] = idx;
     let parentIdx = this.myParentIdx(idx);
@@ -123,15 +125,18 @@ class MinHeap {
 
   // Remove the root (min),
   //   put the last element in the top and then rearrange
-  // return the root and the new arrangement
+  // returns the root and the new arrangement
   dequeue() {
     // if is empty return undefined
-    if (this.values.length === 0) return undefined;
-    if (this.values.length === 1)
+    if (this.size === 0) return undefined;
+    if (this.size === 1) {
+      this.size--;
       return { element: this.values.pop(), heap: this };
+    }
     const min = this.values[0];
     // replace the root with the last element
     this.values[0] = this.values.pop();
+    this.size--;
     // delete from dict
     delete this.idxs[min.key];
     // update idx of the 'new root' in the dict
@@ -139,8 +144,6 @@ class MinHeap {
     // index of this node we have to rearrange and the idx of its children
     let idx = 0;
     let [lChild, rChild] = this.myChildrenIdx(idx);
-    // to keep the smaller
-    let smallIdx;
     // bubble-down (while any child is smaller than the parent)
     while (this.lessThan(lChild, idx) || this.lessThan(rChild, idx)) {
       // update idx and its children
@@ -156,15 +159,17 @@ class MinHeap {
     // check whether this key belongs to this heap
     if (this.values[this.idxs[key]] === undefined) return false;
     // if it is the last element of values: we dont need to rearrange
-    if (this.idxs[key] === this.values.length - 1) {
+    if (this.idxs[key] === this.size - 1) {
       this.values.pop();
       delete this.idxs[key];
+      this.size--;
       return this;
     }
     //   get idx of this key
     let idx = this.idxs[key];
     // replace this node with the last one
     this.values[idx] = this.values.pop();
+    this.size--;
     // delete from dict
     delete this.idxs[key];
     // update idx of the 'new node in idx position' in the dict
