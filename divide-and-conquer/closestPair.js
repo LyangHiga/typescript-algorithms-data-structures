c3e168684ce2c6976d5e1252053bdd5bdee972cf
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-12 15:12:39 -0300

    Debugging closest pair

--- divide-and-conquer/closestPair.js
+++ divide-and-conquer/closestPair.js
@@ -83,28 +83,31 @@ const closestPairBruteForce = (l) => {
   return [pair[0], pair[1], min];
 };
 
+// Returns: [point1, point2, distance] or [null,null, infinity] if dont find any pair
+// inputs: list of points sorted in x and y coordinates, min dist found until now
 const closestSplitPair = (px, py, delta) => {
   const mid = Math.floor(py.length / 2);
   // vertical line splits in 2 sets
   const line = px[mid];
+  // list of points closer to line than delta in x coordinate, sorted by y coordinate
   const sy = [];
   for (let i = 0; i < py.length; i++) {
     // if the distance between the x coordinate of a given point is greater than delta
     // then the distance between this point and anyone else of the other side of this line
-    // would be greater than delta
-    if (Math.abs(py[i].x - line) < delta) {
+    // would be greater than delta, wich is a min dist already found
+    if (Math.abs(py[i].x - line.x) < delta) {
       sy.push(py[i]);
     }
   }
   // if we dont find any candidate
   if (sy.length === 0) return [null, null, Infinity];
   let best = delta;
-  let pair = [];
+  let pair = [null, null];
   let d;
   // search by the best pair
-  //runs at most 6 times
+  //runs at most 6 times => sy.lenght <7
   for (let i = 0; i < sy.length; i++) {
-    for (let j = i + 1; j < 7 && sy[j].y - sy[i] < best; j++) {
+    for (let j = i + 1; j < sy.length; j++) {
       d = dist(sy[i], sy[j]);
       if (d < best) {
         pair = [sy[i], sy[j]];
@@ -115,18 +118,26 @@ const closestSplitPair = (px, py, delta) => {
   return [pair[0], pair[1], best];
 };
 
+// Returns: [point1, point2, distance]
+// input:
+//  px:list of points sorted by x coordinate
+//  py:list of points sorted by y coordinate
 const closestPair = (px, py) => {
   // base case
   if (px.length <= 3) {
     return closestPairBruteForce(px);
   }
-
   const mid = Math.floor(px.length / 2);
+  //   left part sorted by x coordinate
   let qx = [],
+    //   left part sorted by y coordinate
     qy = [],
+    //   right part sorted by x coordinate
     rx = [],
+    //   right part sorted by y coordinate
     ry = [];
 
+  // sorting qx, qy, rx, ry
   for (let i = 0; i < mid; i++) {
     qx.push(px[i]);
   }
@@ -138,43 +149,61 @@ const closestPair = (px, py) => {
 
   const [p1, q1, d1] = closestPair(qx, qy);
   const [p2, q2, d2] = closestPair(rx, ry);
+  // min distance foun until now
   const delta = Math.min(d1, d2);
   const [p3, q3, d3] = closestSplitPair(px, py, delta);
+  // return the min found
   const min = Math.min(d1, d2, d3);
   if (min === d1) return [p1, q1, d1];
   if (min === d2) return [p2, q2, d2];
   return [p3, q3, d3];
 };
 
+// main fuction
+// input: list of points
+// returns: [point1,point2, distance]
 const closestPairList = (l) => {
+  // list l of points sorted by x coordinate
   const px = mergeSort(l, true);
+  // list l of points sorted by y coordinate
   const py = mergeSort(l, false);
   return closestPair(px, py);
 };
 
+// gives a list of n points in range of x:(-n,+n) and y:(-r,+r)
 const getPoints = (n, r) => {
   const list = [];
-  const check = {};
   let i = 0;
   let x;
-  let val;
   while (i < n) {
     if (Math.floor(Math.random() * 10) > 4) x = -1;
     else x = 1;
-    val = Math.floor(x * i * Math.random());
-    while (check[val] === true) {
-      val = Math.floor(x * i * Math.random());
-    }
-    check[val] = true;
-    list.push(new Point(val, Math.floor(Math.random() * r * x), i.toString()));
+    list.push(
+      new Point(i * x, Math.floor(Math.random() * r * x), i.toString())
+    );
     i++;
   }
   return list;
 };
 
-const list = getPoints(3000, 1000);
-console.log('*******************************');
-const [p0, q0, d0] = closestPairList(list);
-console.log(`(${p0.name}, ${q0.name}): ${d0}`);
-const [p1, q1, d1] = closestPairBruteForce(list);
-console.log(`(${p1.name}, ${q1.name}): ${d1}`);
+// number of times to test, size of ths list of points,
+// range of random numbers
+const test = (nTimes, lSize, random) => {
+  let err = 0;
+  let list = getPoints(lSize, random);
+  let [p0, q0, d0] = closestPairList(list);
+  console.log(`(${p0.name}, ${q0.name}): ${d0}`);
+  let [p1, q1, d1] = closestPairBruteForce(list);
+  console.log(`(${p1.name}, ${q1.name}): ${d1}`);
+  if (d0 !== d1) err++;
+  console.log('*******************************');
+  for (let i = 0; i < nTimes; i++) {
+    list = getPoints(lSize, random);
+    [p0, q0, d0] = closestPairList(list);
+    [p1, q1, d1] = closestPairBruteForce(list);
+    if (d0 !== d1) err++;
+  }
+  console.log(`err = ${err}`);
+};
+
+test(1000, 1000, 1000);
