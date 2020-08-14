1e8f6058a5d07064480640a5e6f453841682cc2b
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-14 14:32:54 -0300

    Refactoring quicksort

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,19 +1,91 @@
 const { performance } = require('perf_hooks');
-// returns start <= random < end
-function random(min, max) {
+
+// Returns a random number between [min,max)
+const random = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-// output: the index of the pivot
-function partition(arr, start = 0, end = arr.length) {
+// Returns  the median of a list with 3 values
+const medianOf3 = (l) => {
+  l.sort(function (a, b) {
+    return a - b;
+  });
+  return l[1];
+};
+
+// Returns the pivot, and if it's not the first element of the arr, it swaps
+// chooseP:
+//      0: the First Element
+//      1: the last Element
+//      2: the median of 3 (first, middle, last) elements
+//      3: Random
+//      4: the median of 3 random elements in range [start,end)
+const choosePivot = (arr, start = 0, end = arr.length, chooseP = 2) => {
+  switch (chooseP) {
+    case 0:
+      return arr[start];
+    case 1:
+      const last = end - 1;
+      const p = arr[last];
+      // swap pivot to the first position
+      [arr[start], arr[last]] = [arr[last], arr[start]];
+      return p;
+    case 2:
+      const l = [];
+      const first = arr[start];
+      l.push(first);
+      const lastIdx = end - 1;
+      const lastEle = arr[lastIdx];
+      l.push(lastEle);
+      const middleIdx = Math.floor((lastIdx - start) / 2) + start;
+      const middle = arr[middleIdx];
+      l.push(middle);
+      const median = medianOf3(l);
+      if (median === lastEle) {
+        // swap pivot (last Element) to the first position, and return the pivot
+        [arr[start], arr[lastIdx]] = [arr[lastIdx], arr[start]];
+      }
+      if (median === middle) {
+        // swap pivot (middle Element) to the first position, then return the pivot
+        [arr[start], arr[middleIdx]] = [arr[middleIdx], arr[start]];
+      }
+      return median;
+    case 3:
+      const idx = random(start, end);
+      [arr[start], arr[idx]] = [arr[idx], arr[start]];
+      return arr[idx];
+    case 4:
+      const idx1 = random(start, end);
+      const idx2 = random(start, end);
+      const idx3 = random(start, end);
+      const list = [];
+      list.push(arr[idx1], arr[idx2], arr[idx3]);
+      const m = medianOf3(list);
+      if (m === arr[idx1]) {
+        // swap pivot (last Element) to the first position, and return the pivot
+        [arr[start], arr[idx1]] = [arr[idx1], arr[start]];
+      }
+      if (m === arr[idx2]) {
+        // swap pivot (last Element) to the first position, and return the pivot
+        [arr[start], arr[idx2]] = [arr[idx2], arr[start]];
+      }
+      if (m === arr[idx3]) {
+        // swap pivot (last Element) to the first position, and return the pivot
+        [arr[start], arr[idx3]] = [arr[idx3], arr[start]];
+      }
+      return m;
+  }
+};
+
+// Returns: the order statistic of a given pivot (its correct position if the arr was ordered)
+// also make a partition around the pivot
+// puts the pivot in its right position
+// and all elements that are smaller than pivot in its left
+// and all elements that are greater than pivot in its right
+const partition = (arr, start = 0, end = arr.length, chooseP = 2) => {
   // lets take the first element as pivot
-  // const p = arr[start];
-  // random pivot
-  const index = random(start, end);
-  const p = arr[index];
-  // swap pivot to the first position
-  [arr[start], arr[index]] = [arr[index], arr[start]];
-
+  //   const p = arr[start];
+  const p = choosePivot(arr, start, end, chooseP);
   // keep track of how many elements are smaller than the pivot, to mark its right position
   let i = start + 1;
   // elements with idx < j : partitioned
@@ -29,29 +101,39 @@ function partition(arr, start = 0, end = arr.length) {
   // put p in its right position and return this idx
   [arr[start], arr[i - 1]] = [arr[i - 1], arr[start]];
   return i - 1;
-}
+};
 
-function quickSort(arr, left = 0, right = arr.length) {
-  if (right - left < 1) return;
-  // find the right position of a random pivot
-  const p = partition(arr, left, right);
+// Returns a sorted array and the number of comparisons
+// Inputs:
+//  arr: unsorted array
+//  left: index of first element to sort
+//  right: index of the last element
+const quickSort = (arr, left = 0, right = arr.length, chooseP = 2) => {
+  if (right - left < 1) return [arr, 0];
+  // to count the number of comparisons
+  const comp = right - left - 1;
+  let cl, cr;
+  // find the right position of a given pivot
+  const p = partition(arr, left, right, chooseP);
   // recurssive through p
-  quickSort(arr, left, p);
-  quickSort(arr, p + 1, right);
-  return arr;
-}
+  [arr, cl] = quickSort(arr, left, p, chooseP);
+  [arr, cr] = quickSort(arr, p + 1, right, chooseP);
+  return [arr, comp + cl + cr];
+};
 
-// console.log(quickSort([10, -9, 44, 56, 49, 400, -94, 34, 56, 0]));
+const test = (arrSize, nTimes) => {
+  const data = Array.apply(null, { length: arrSize }).map(
+    Function.call,
+    Math.random
+  );
 
-let data = Array.apply(null, { length: 120000 }).map(
-  Function.call,
-  Math.random
-);
+  const t1 = performance.now();
+  for (let i = 0; i < nTimes; i++) {
+    quickSort(data);
+  }
 
-let t1 = performance.now();
-for (let i = 0; i < 10; i++) {
-  quickSort(data);
-}
+  const t2 = performance.now();
+  console.log('Average time: ' + (t2 - t1) / (1000 * nTimes) + ' seconds');
+};
 
-let t2 = performance.now();
-console.log('Average time: ' + (t2 - t1) / 10000 + ' seconds');
+module.exports = quickSort;
