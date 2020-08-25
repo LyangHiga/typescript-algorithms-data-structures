abcf2d0e9156b0265313500201db3a05de7c9b82
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-25 16:54:41 -0300

    Refactoring maxHeap with Node class

--- data-structures/maxBinaryHeap.js
+++ data-structures/maxBinaryHeap.js
@@ -1,81 +1,76 @@
-class MaxBinaryHeap{
-    constructor(){
-        this.values = [];
-    }
-
-    // return if the element from index i is greater than k idx element
-    greaterThan(i,k){
-        // out of bounds
-        if(i < 0||k <0 ) return false;
-        if(i > this.values.length - 1 || k > this.values.length - 1) return false;
-        let val = this.values[i];
-        let parent = this.values[k];
-        if(val > parent) return true;
-        return false;
-    }
-
-    // insert an element in the next free spot and then sort the Heap if it's needed
-    insert(val){
-        this.values.push(val);
-        let idx = this.values.length - 1;
-        let parentIdx = Math.floor( (idx-1)/2);
-        // sort
-        while(this.greaterThan(idx,parentIdx)){
-            //swap
-            [this.values[idx],this.values[parentIdx]] = [this.values[parentIdx],this.values[idx]];
-            idx = parentIdx;
-            parentIdx = Math.floor((idx-1)/2);
-        }
-        return this;
-    }
-
-    // Remove the root (max), put the last element in the top and then rearrange
-    // return the root and the new arrangement
-    remove(){
-        // if is empty return undefined
-        if(this.values.length === 0 ) return undefined;
-        if(this.values.length === 1  ) return {element:this.values.pop(),heap:this};
-        const max = this.values[0];
-        // replace the root with the last element
-        this.values[0] = this.values.pop();
-        let idx = 0;
-        let lChild = (2 * idx) + 1;
-        let rChild = (2 * idx) + 2;
-        let greatIdx;
-        while(this.greaterThan(lChild,idx) || this.greaterThan(rChild,idx)){
-            if(this.greaterThan(lChild,rChild)){
-                greatIdx = lChild;
-            } else {
-                greatIdx = rChild;
-            }
-            // swap element from idx with greater
-            [this.values[idx],this.values[greatIdx]] = [this.values[greatIdx],this.values[idx]];
-            // update idx and its children
-            idx = greatIdx;
-            lChild = (2 * idx) + 1;
-            rChild = (2 * idx) + 2;
-        }
-        return {element:max,heap:this};
-    }
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+  }
 }
 
-let heap = new MaxBinaryHeap();
-console.log(heap.insert(41));
-// console.log(heap.insert(39));
-// console.log(heap.insert(18));
-// console.log(heap.insert(27));
-// console.log(heap.insert(12));
-// console.log(heap.insert(55));
-// console.log(heap.insert(33));
-// console.log(heap.insert(1));
-// console.log(heap.insert(45));
-// console.log(heap.insert(200));
-console.log(heap.remove());
-console.log(heap.remove());
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
 
+  // return if the element from index i is greater than k idx element
+  greaterThan(i, k) {
+    // out of bounds
+    if (i < 0 || k < 0) return false;
+    if (i > this.values.length - 1 || k > this.values.length - 1) return false;
+    if (this.values[i].val > this.values[k].val) return true;
+    return false;
+  }
 
-//              55
-//        45          41
-//    39     12    18   33
-// 1     27
+  // Inserts a node (key,val) in the last position and rearrange
+  // Returns Heap
+  insert(key, val) {
+    let node = new Node(key, val);
+    this.values.push(node);
+    // insert in the last position
+    let idx = this.values.length - 1;
+    let parentIdx = Math.floor((idx - 1) / 2);
+    // sort
+    while (this.greaterThan(idx, parentIdx)) {
+      //swap
+      [this.values[idx], this.values[parentIdx]] = [
+        this.values[parentIdx],
+        this.values[idx],
+      ];
+      idx = parentIdx;
+      parentIdx = Math.floor((idx - 1) / 2);
+    }
+    return this;
+  }
 
+  // Removes the root (max),
+  // Returns the root and the new arrangement
+  // Returns null if this heap is empty
+  dequeue() {
+    // if is empty return undefined
+    if (this.values.length === 0) return null;
+    if (this.values.length === 1)
+      return { element: this.values.pop(), heap: this };
+    const max = this.values[0];
+    // replace the root with the last element
+    this.values[0] = this.values.pop();
+    let idx = 0;
+    let lChild = 2 * idx + 1;
+    let rChild = 2 * idx + 2;
+    let greatIdx;
+    while (this.greaterThan(lChild, idx) || this.greaterThan(rChild, idx)) {
+      if (this.greaterThan(lChild, rChild)) {
+        greatIdx = lChild;
+      } else {
+        greatIdx = rChild;
+      }
+      // swap element from idx with greater
+      [this.values[idx], this.values[greatIdx]] = [
+        this.values[greatIdx],
+        this.values[idx],
+      ];
+      // update idx and its children
+      idx = greatIdx;
+      lChild = 2 * idx + 1;
+      rChild = 2 * idx + 2;
+    }
+    return { element: max, heap: this };
+  }
+}
