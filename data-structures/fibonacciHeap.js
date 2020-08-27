60e379231faddfce76f614001c3c71436124d32d
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-27 19:38:23 -0300

    Add union method to FH

--- data-structures/fibonacciHeap.js
+++ data-structures/fibonacciHeap.js
@@ -48,6 +48,18 @@ class FibonacciHeap {
     }
   }
 
+  // concatenate this min root list with root list of a given node( min of h2 = min2)
+  // [...] <-> min1 <-> t1 <-> [...], [...] <-> t2 <-> min2 <-> [...]
+  // [...] t2 <-> t1 <->[...] <-> min1 <-> min2 <-> [...]
+  concatenate(node) {
+    const t1 = this.min.right;
+    const t2 = node.left;
+    this.min.right = node;
+    node.left = this.min;
+    t1.left = t2;
+    t2.right = t1;
+  }
+
   //   **********************************************************
   //                            ACCESSING
   //   **********************************************************
@@ -71,5 +83,23 @@ class FibonacciHeap {
     }
     this.size++;
     return this;
+  }
+
+  //   **********************************************************
+  //                            UNION
+  //   **********************************************************
+
+  // Returns a new FH h, wich is the union between this FH and h2
+  // Returns false if any FH is empty
+  union(h2) {
+    if (this.isEmpty() || h2.isEmpty()) return false;
+    const h = new FibonacciHeap();
+    h.min = this.min;
+    h.concatenate(h2.min);
+    if (h2.min.val < this.min.val) {
+      h.min = h2.min;
+    }
+    h.size = this.size + h2.size;
+    return h;
   }
 }
