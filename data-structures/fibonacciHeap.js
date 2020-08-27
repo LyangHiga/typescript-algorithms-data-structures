c75e172139e487e87a0278b94f6d2f1dd66c2ad1
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-27 18:18:56 -0300

    Add enqueue and helpers to FH

--- data-structures/fibonacciHeap.js
+++ data-structures/fibonacciHeap.js
@@ -18,4 +18,46 @@ class FibonacciHeap {
     this.size = 0;
     this.min = null;
   }
+
+  //   **********************************************************
+  //                            HELPERS
+  //   **********************************************************
+
+  // Returns true if this heap is empty
+  // Otherwise returns false
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // Adds a new node to the root list
+  // [...] <-> t <-> min <-> [...]   adds  node
+  // [...] <-> t <- node -> <- min <-> [...]
+  // If this FH is empty node become min and points to itself
+  //    An only child points to itself in left and right
+  addToRootList(node) {
+    if (this.isEmpty()) {
+      this.min = node;
+      this.min.left = node;
+      this.min.right = node;
+    } else {
+      const t = this.min.left;
+      this.min.left = node;
+      node.left = t;
+      t.right = node;
+      node.right = this.min;
+    }
+  }
+
+  //   **********************************************************
+  //                            INSERT
+  //   **********************************************************
+  enqueue(key, val) {
+    const node = new Node(key, val);
+    this.addToRootList(node);
+    if (node.val < this.min.val) {
+      this.min = node;
+    }
+    this.size++;
+    return this;
+  }
 }
