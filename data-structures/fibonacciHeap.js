8b79aa4f39c0c96d2d8c1aca74e98cb209930945
lyangHiga <lyang_higa@poli.ufrj.br>
2020-08-29 17:03:08 -0300

    Add consolidate to FH

--- data-structures/fibonacciHeap.js
+++ data-structures/fibonacciHeap.js
@@ -120,6 +120,63 @@ class FibonacciHeap {
     y.mark = false;
   }
 
+  // Ensures that every root has distinct degree
+  //    Search roots with same degree and link
+  //    until there is at most one root with each degree
+  // O(maxDegree) = O(log n)
+  consolidate() {
+    //   max degree := log(n)
+    const maxDegree = Math.log2(this.size);
+    // to keep track the root's degree
+    const a = new Array(maxDegree).fill(null);
+    // for each node w in the root list
+    const w = this.min;
+    // removing pointer from left sibling of min to min
+    // left <-> min <-> [...]
+    // left <- min <-> [...]
+    this.min.left.right = null;
+    while (w !== null) {
+      const x = w;
+      const d = x.degree;
+      const next = x.right;
+      // in each step we remove the pointer to the right sibling
+      w.right = null;
+      // check if there is any other root with the same degree d
+      while (a[d] !== null) {
+        // another root (besides x) with the same degree d
+        const y = a[d];
+        if (y.val < x.val) {
+          // exhange pointers, x MUST be the smaller
+          [x, y] = [y, x];
+        }
+        this.link(x, y);
+        // after link x and y, y is child of x, and x has degree d + 1
+        a[d] = null;
+        d += 1;
+      }
+      // a[d] points to x
+      a[d] = x;
+      // update w
+      w = next;
+    }
+    // Reconstructing Root list
+    this.min = null;
+    for (let i = 0; i < maxDegree; i++) {
+      if (a[i] !== null) {
+        // add node a[i] to the root list
+        this.addToRootList(a[i]);
+        // update min
+        if (this.min === null) {
+          this.min = a[i];
+        } else {
+          if (a[i].val < this.min.val) {
+            this.min = a[i];
+          }
+        }
+      }
+    }
+  }
+
   //   **********************************************************
   //                            ACCESSING
   //   **********************************************************
